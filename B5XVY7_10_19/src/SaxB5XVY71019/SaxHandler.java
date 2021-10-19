08d870296305b0efbc72bd408b3900e9a86dd5e6
Szabó Gergely <grg.szabogergo@gmail.com>
2021-10-19 15:10:18 +0200

    Add files via upload

--- B5XVY7_10_19/src/SaxB5XVY71019/SaxHandler.java
+++ B5XVY7_10_19/src/SaxB5XVY71019/SaxHandler.java
@@ -1,6 +1,7 @@
 package SaxB5XVY71019;
 
 import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SaxHandler  extends DefaultHandler {
@@ -26,6 +27,31 @@ public class SaxHandler  extends DefaultHandler {
 	private void indent() {
 		for (int i = 0; i < indent; i++) {
 			System.out.print(" ");
+		}
+	}
+	
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		indent++;
+		indent();
+		System.out.println(qName + formatAttribute(attributes) + " start");
+	}
+	
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		indent();
+		indent--;
+		System.out.println(qName + " end");
+	}
+	
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		String chars = new String(ch, start, length).trim();
+		if(!chars.isEmpty()) {
+			indent++;
+			indent();
+			indent--;
+			System.out.println(chars);
 		}
 	}
 	
